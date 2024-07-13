1edd3368392094954f6dd715001946062bf8d5ce
cogito1016 <cogito1016@naver.com>
2024-07-14 02:44:30 +0900

    feat: 인증 > passportModule, JwtStrategy 적재

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
