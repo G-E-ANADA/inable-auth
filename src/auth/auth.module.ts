6ffef041aa57c924595425e95276fcd5067bc636
cogito1016 <cogito1016@naver.com>
2024-07-14 03:08:41 +0900

    feat: 로그인기능 구현 > guard를 활용한 jwt 검증 로직 적용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [UsersModule, PassportModule],
   providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
